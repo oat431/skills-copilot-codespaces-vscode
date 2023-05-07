f15209f20bfd55d80823cff3cd4d38822e85a1ea
Oralita <54722120+oat431@users.noreply.github.com>
2023-05-07 18:04:18 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+    // console.log("skillsMember");
+    var member = document.getElementById("member");
+    member.style.display = "block";
+    var skills = document.getElementById("skills");
+    skills.style.display = "none";
+    var projects = document.getElementById("projects");
+    projects.style.display = "none";
+    var contact = document.getElementById("contact");
+    contact.style.display = "none";
+    var about = document.getElementById("about");
+    about.style.display = "none";
+}
